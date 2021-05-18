f3b20355d9082fdd5b597b32ce002d16856d5e2a
Keisuke Nakayama <me@yammer.jp>
2021-05-18 12:37:29 +0900

    Add to fetch tasks from mock api

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,40 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{
+      tasksComputed
+    }}
   </div>
 </template>
 
 <script>
+/* import axios from 'axios'; */
 import HelloWorld from './components/HelloWorld.vue';
+import store from './store/index';
 
 export default {
   name: 'App',
+  store,
+  data: () => ({
+    tasks: [],
+  }),
   components: {
     HelloWorld,
+  },
+  mounted() {
+    this.$store.dispatch('getTasksAction');
+    /*
+    axios.get('http://localhost:3001/tasks').then(({ data }) => {
+      console.log(data);
+    });
+    console.log('hel');
+    console.log(this.$store.state.tasks);
+    */
+  },
+  computed: {
+    tasksComputed() {
+      return this.$store.getters.getTasks();
+    },
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Vue from 'vue';
 import Vuex from 'vuex';
 
@@ -5,10 +6,59 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    tasks: [],
+  },
+  getters: {
+    getTasks: (state /* , getters */) => () => state.tasks,
   },
   mutations: {
+    updateTasks(state, tasks) {
+      state.tasks = tasks;
+    },
+    getTasks(state, tasks) {
+      state.tasks = tasks;
+    },
   },
   actions: {
+    async getTasksAction(context) {
+      const res = await axios.get('http://localhost:3002/tasks');
+      if (res.status !== 200) {
+        console.error('get tasks is failed');
+        console.error(res);
+        return;
+      }
+      if (!res.data.length) {
+        console.error('get tasks is got, but it is not array');
+        console.error(res);
+        return;
+      }
+      const tasks = res.data.map(({
+        id, title, description, done,
+      }) => {
+        if (typeof id !== 'number') {
+          console.error(`type validation failed. id: ${id} is not number...`);
+          return {};
+        }
+        if (typeof title !== 'string') {
+          console.error(`type validation failed. title: ${title} is not string...`);
+          return {};
+        }
+        if (typeof description !== 'string') {
+          console.error(`type validation failed. description: ${description} is not string...`);
+          return {};
+        }
+        if (typeof done !== 'boolean') {
+          console.error(`type validation failed. done: ${done} is not boolean...`);
+          return {};
+        }
+        return {
+          id, title, description, done,
+        };
+      }).filter(({ id }) => id !== undefined);
+      console.log(tasks);
+      // context.commit('getTasks', res.data);
+      context.commit('getTasks', tasks);
+    },
   },
   modules: {
   },
